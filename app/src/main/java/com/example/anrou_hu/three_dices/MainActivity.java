c0d1872d9717727253a332f880651c8d443eb6ba
anrou_hu <anrou_hu@hiiir.com>
2017-08-09 18:11:08 +0800

    @update basic settings

---
+++ app/src/main/java/com/example/anrou_hu/three_dices/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.anrou_hu.three_dices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/anrou_hu/three_dices/MainViewModel.java
@@ -0,0 +1,8 @@
+package com.example.anrou_hu.three_dices;
+
+/**
+ * @author anrou_hu
+ */
+
+public class MainViewModel extends ViewModel {
+}
