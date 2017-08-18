6c96369e99023c0a56246ba4eb3f3a1647add176
anrou_hu <anrou_hu@hiiir.com>
2017-08-18 15:14:08 +0800

    @update add LiveData and Livecycle

--- app/src/main/java/com/example/anrou_hu/three_dices/MainActivity.java
+++ app/src/main/java/com/example/anrou_hu/three_dices/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.anrou_hu.three_dices;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleActivity;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends LifecycleActivity implements View.OnClickListener {
+
+    private TextView mFirstDice;
+    private TextView mSecondDice;
+    private TextView mThirdDice;
+    private TextView mPoint;
+    private Button mRoll;
+    private RecyclerView mResult;
+
+    private MainViewModel mViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        initViewModel();
+        initObserver();
+    }
+
+    private void initViews() {
+        mFirstDice = (TextView) findViewById(R.id.firstDice);
+        mSecondDice = (TextView) findViewById(R.id.secondDice);
+        mThirdDice = (TextView) findViewById(R.id.thirdDice);
+        mPoint = (TextView) findViewById(R.id.point);
+        mRoll = (Button) findViewById(R.id.roll);
+        mResult = (RecyclerView) findViewById(R.id.frequencyTable);
+        mRoll.setOnClickListener(this);
+    }
+
+
+    private void initViewModel() {
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+    }
+
+    private void initObserver() {
+        Lifecycle lifecycle = getLifecycle();
+        lifecycle.addObserver(mViewModel);
+
+        mViewModel.getDicePoints().observe(this, new Observer<int[]>() {
+            @Override
+            public void onChanged(@Nullable int[] ints) {
+                updateDicePoints(ints);
+            }
+        });
+    }
+
+    private void updateDicePoints(int[] dicePoints) {
+        int dice1 = dicePoints[0];
+        int dice2 = dicePoints[1];
+        int dice3 = dicePoints[2];
+        int totalPoints = dice1 + dice2 + dice3; //do it in view model or Live data
+        mFirstDice.setText(String.valueOf(dice1));
+        mSecondDice.setText(String.valueOf(dice2));
+        mThirdDice.setText(String.valueOf(dice3));
+        mPoint.setText(String.valueOf(totalPoints));
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.roll:
+                rollDices();
+                break;
+        }
+    }
+
+    private void rollDices() {
+        mViewModel.rollDices();
     }
 }
--- app/src/main/java/com/example/anrou_hu/three_dices/MainViewModel.java
+++ app/src/main/java/com/example/anrou_hu/three_dices/MainViewModel.java
@@ -1,8 +1,72 @@
 package com.example.anrou_hu.three_dices;
 
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.OnLifecycleEvent;
+import android.arch.lifecycle.ViewModel;
+import android.util.Log;
+
+import java.util.List;
+
 /**
  * @author anrou_hu
  */
 
-public class MainViewModel extends ViewModel {
+public class MainViewModel extends ViewModel implements LifecycleObserver, AsyncTaskCallback {
+
+    private final static int DICE_COUNT = 3;
+
+    private MutableLiveData<Integer> mTotalPoint = new MutableLiveData<>();
+    private MutableLiveData<int[]> mDicePoints = new MutableLiveData<int[]>() {};
+
+
+
+    public LiveData<int[]> getDicePoints() {
+        return mDicePoints;
+    }
+
+
+    void rollDices() {
+        DiceRollingAsyncTask task = new DiceRollingAsyncTask(this, DICE_COUNT);
+        task.execute();
+    }
+
+
+
+    @Override
+    public void processFinish(Object result) {
+        int[] diceList = (int[]) result;
+        mDicePoints.setValue(diceList);
+
+        int totalPoint = calculateTotalPoint(diceList);
+        mTotalPoint.setValue(totalPoint);
+    }
+
+    private int calculateTotalPoint(int[] diceList) {
+        int totalPoint = 0;
+        for (int dicePoint : diceList) {
+            totalPoint += dicePoint;
+        }
+        return totalPoint;
+    }
+
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    void create() {
+        Log.d("Lifecycle", "Test Lifecycle : on create~");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    void start() {
+        Log.d("Lifecycle", "Test Lifecycle : on start~");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    void stop() {
+        Log.d("Lifecycle", "Test Lifecycle : on stop~");
+    }
+
+
 }
